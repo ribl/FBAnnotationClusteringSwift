cbb805e46c5a9ba1fb67fa6b9ba2b5915877bc1b
javialgaba <javialgaba@gmail.com>
2016-04-04 11:01:50 +0200

    Carthage support

---
+++ Example/Pods/Target Support Files/Pods-FBAnnotationClusteringSwift_Example/Pods-FBAnnotationClusteringSwift_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FBAnnotationClusteringSwift_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FBAnnotationClusteringSwift_ExampleVersionString[];
+
